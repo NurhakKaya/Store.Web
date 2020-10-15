b82642e497fd81700e8c033f443e64acffb53019
Nurhak Kaya <Nurhak.Kaya@felinesoft.com>
2020-10-15 20:54:21 +0100

    final changes for data access layer

---
+++ Store.Web/Store.Data/Repositories/CategoryRepository.cs
@@ -0,0 +1,36 @@
+﻿using Store.Data.Infrastructure;
+using Store.Model;
+using System;
+using System.Linq;
+
+namespace Store.Data.Repositories
+{
+    /// <summary>
+    /// CategoryRepository extends default operations (GetCategoryByName) and overrides teh default Update operation.
+    /// We should add a repository for each of Model classes, hence each repository of tyep T is responsible to manipulate a specific DbSet through
+    /// the DbContext.Set<T>.
+    /// </summary>
+    public class CategoryRepository : RepositoryBase<Category>, ICategoryRepository
+    {
+        public CategoryRepository(IDbFactory dbFactory)
+            : base(dbFactory) { }
+
+        public Category GetCategoryByName(string categoryName)
+        {
+            var category = this.DbContext.Categories.Where(c => c.Name == categoryName).FirstOrDefault();
+
+            return category;
+        }
+
+        public override void Update(Category entity)
+        {
+            entity.DateUpdated = DateTime.Now;
+            base.Update(entity);
+        }
+    }
+
+    public interface ICategoryRepository : IRepository<Category>
+    {
+        Category GetCategoryByName(string categoryName);
+    }
+}
---
+++ Store.Web/Store.Data/Repositories/GadgetRepository.cs
@@ -0,0 +1,19 @@
+﻿using Store.Data.Infrastructure;
+using Store.Model;
+
+namespace Store.Data.Repositories
+{
+    /// <summary>
+    /// GadgetRepository supports the default operations using the default behaviour
+    /// </summary>
+    public class GadgetRepository : RepositoryBase<Gadget>, IGadgetRepository
+    {
+        public GadgetRepository(IDbFactory dbFactory)
+            : base(dbFactory) { }
+    }
+
+    public interface IGadgetRepository : IRepository<Gadget>
+    {
+
+    }
+}
